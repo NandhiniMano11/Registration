d4c548843ac8b668c70741680a6571c9012c162c
nandhini <nandhinimano11@gmail.com>
2021-02-04 18:18:01 +0530

    Updates

---
+++ precache-manifest.8b84b74af32bb3788413c827da3d450f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66030d84414e3debffa5f05538cc786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48dbfca448209f09803d",
+    "url": "/static/css/main.70c3f86f.chunk.css"
+  },
+  {
+    "revision": "44025e32d597670f8f3f",
+    "url": "/static/js/2.93998146.chunk.js"
+  },
+  {
+    "revision": "48dbfca448209f09803d",
+    "url": "/static/js/main.d9071726.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
